b06e19d4fbecb801a82a95d6e6d72a163b51d9c6
Amasha2000 <amashagalhenage@gmail.com>
2022-08-14 13:18:08 +0530

    Content Updated

--- src/controller/ServerFormController.java
+++ src/controller/ServerFormController.java
@@ -7,8 +7,10 @@ import java.net.Socket;
 public class ServerFormController {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(3000);
-        Socket clientSocket = serverSocket.accept();
-
-        System.out.println("Client Connected");
+        
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client Connected");
+        }
     }
 }
