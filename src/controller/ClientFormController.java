3114e48b203c53a29c355d2437395c1ba3268a35
Amasha2000 <amashagalhenage@gmail.com>
2022-08-14 13:04:27 +0530

    Content updated in controllers

--- src/controller/ClientFormController.java
+++ src/controller/ClientFormController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientFormController {
+    public Label lblName;
+    public JFXTextArea clientTextArea;
+    public JFXTextField txtClient;
+    public JFXButton btnClient;
+
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost",3000);
+
+    }
+
+    public void sendOnAction(ActionEvent actionEvent) {
+
+    }
 }
--- src/controller/ServerFormController.java
+++ src/controller/ServerFormController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerFormController {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(3000);
+        Socket clientSocket = serverSocket.accept();
+
+        System.out.println("Client Connected");
+    }
 }
